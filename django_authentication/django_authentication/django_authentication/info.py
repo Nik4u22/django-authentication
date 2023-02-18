ed781cc22ef8832c4dbf3be7b30a88d53238bc61
Nikhil Jagnade <64134540+Nik4u22@users.noreply.github.com>
2023-02-18 13:51:21 +0530

    Add files via upload

---
+++ django_authentication/django_authentication/django_authentication/info.py
@@ -0,0 +1,9 @@
+EMAIL_USE_TLS = True
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_HOST_USER = 'your email id'
+
+EMAIL_HOST_PASSWORD = 'your password' # Generate app password from google account settings->security->app
+
+EMAIL_PORT = 587
